b13143a185a7c44931a11011e3c2071414df4547
MossRass <mostafarasmouki5@gmail.com>
2023-06-15 16:42:29 -0700

    the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - A C program for the standard error
+ *
+ *Retrun: Always 1 (success)
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	return (1);
+}
