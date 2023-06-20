e87dbe346d9787d053c01623db7c732972caeea0
MossRass <mostafarasmouki5@gmail.com>
2023-06-20 11:38:23 -0700

    _islower - check if char is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ *
+ * @c: the character to be checked
+ *
+ * Return 1 if lettre ,otherwise 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1)
+	else if (c >= 'A' && c <= 'Z')
+		return (1)
+	else
+		return(0)
+}
