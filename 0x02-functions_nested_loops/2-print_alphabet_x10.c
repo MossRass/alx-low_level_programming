621b24aaa86861e787e1c538a0fc89f414f31cc0
MossRass <mostafarasmouki5@gmail.com>
2023-06-20 08:58:49 -0700

    Write a program that prints alphabet in 10lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet  - print_alphabet_x10
+ *
+ * description: alphabet a to z
+*/
+void print_alphabet(void)
+{
+	char alphabet;
+	int line;
+
+	for (line  = 1 ; line <= 10 ; line++)
+	{
+		for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+			_putchar(alphabet);
+	}
+	_putchar('\n');
+}
