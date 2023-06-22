a3b578ee2a2da8dd1b2194268bcfe1e8af5d6a54
MossRass <mostafarasmouki5@gmail.com>
2023-06-22 10:04:23 -0700

    numbers, from 0 to 9, followed by a new line skip 2 && 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 	for (num = '0' ; num < '9' ; num++)
 	{
 		_putchar(num);
-		if (num == '2' || num == '4')
+		if (!(num == '2' || num == '4'))
 		{
 			continue;
 			_putchar(num);
