c8598f3dcc34dc3b7dd777365786b3c4d80dc1fa
MossRass <mostafarasmouki5@gmail.com>
2023-08-31 12:55:45 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,8 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec-val = 0;
+
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
@@ -17,8 +18,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec-val = 2 * dec-val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (dec-val);
+	return (dec_val);
 }
